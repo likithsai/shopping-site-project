fe38abe3f02614f844e3a971ae12d15a6c16ca28
likith sai <likithsai13@gmail.com>
2023-10-04 11:48:59 +0530

    font customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['"lato"', ...defaultTheme.fontFamily.sans],
+    },
   },
   plugins: [],
 }
