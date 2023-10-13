cacf0b1bf317d07154dc59d7d4d21404e73d791f
likith sai <likithsai13@gmail.com>
2023-10-13 11:27:10 +0530

    cart and shop item features

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React from 'react';
+import { connect } from "react-redux";
+import { appConstants } from "../enum/constants";
 
 const Cart = (props) => {
+    const removeFromCart = (itemId) => {
+        props.removeFromCart(itemId);
+    }
+
     return (
-        <p>Cart</p>
-    )
+        <div>
+            <ul>
+                {props.cart.map(cartItem => (
+                    <li key={cartItem.id}>
+                        <p>{ JSON.stringify(cartItem)}</p>
+                        {cartItem.name} - ${cartItem.newprice}
+                        <button onClick={() => removeFromCart(cartItem.id)}>Remove from Cart</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
-export default Cart;
+function mapStateToProps(state) {
+    return {
+        cart: state.cart
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        removeFromCart: (itemId) => dispatch({ type: appConstants.REMOVE_FROM_CART, payload: itemId })
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Cart);
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,24 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from "react-redux";
+import { appConstants } from "../enum/constants";
 
 const Shop = (props) => {
-    const [items, setItems] = useState({});
+    const [items, setItems] = useState([]);
 
     useEffect(() => {
         setItems(props.items);
     }, [props.items]);
 
+    const addToCart = (item) => {
+        props.addToCart(item);
+    }
+
     return (
-        <p>{ JSON.stringify(items) }</p>
+        <div>
+            <ul>
+                {items.map(item => (
+                    <li key={item.id}>
+                        {item.name} - ${item.newprice}
+                        <button onClick={() => addToCart(item)}>Add to Cart</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+
     );
 }
 
 function mapStateToProps(state) {
   return {
-       items: state.items
+        items: state.items.products
   }
 }
 
+function mapDispatchToProps(dispatch) {
+    return {
+        addToCart: (item) => dispatch({ type: appConstants.ADD_TO_CART, payload: item })
+    }
+}
+
 export default connect(
-    mapStateToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(Shop)
