6ac607a0f7ad15390fb8bab901835fa47097fbd3
likith sai <likithsai13@gmail.com>
2023-10-23 10:56:28 +0530

    formatted code

--- src/component/SnapCarousal.js
+++ src/component/SnapCarousal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { BsArrowRightCircle, BsArrowLeftCircle } from "react-icons/bs";
 
 const SnapCarousel = ({ title, children, showMoreLink }) => {
@@ -21,11 +21,16 @@ const SnapCarousel = ({ title, children, showMoreLink }) => {
 
   return (
     <div className="w-full flex flex-col gap-2">
-      <div className='flex items-center justify-between'>
-        {title && <p className="text-lg font-semibold mb-2 font-bold">{title}</p>}
+      <div className="flex items-center justify-between">
+        {title && (
+          <p className="text-lg font-semibold mb-2 font-bold">{title}</p>
+        )}
         {showMoreLink && (
           <div className="mt-2 flex justify-end">
-            <a href={showMoreLink} className="text-blue-500 hover:underline focus:outline-none flex items-center gap-2">
+            <a
+              href={showMoreLink}
+              className="text-blue-500 hover:underline focus:outline-none flex items-center gap-2"
+            >
               <span>Show More</span>
               <BsArrowRightCircle />
             </a>
@@ -33,23 +38,30 @@ const SnapCarousel = ({ title, children, showMoreLink }) => {
         )}
       </div>
       <div className="relative flex items-center overflow-hidden">
-        <div
-          className="flex gap-2 transition-transform duration-300 ease-in-out"
-        >
+        <div className="flex gap-2 transition-transform duration-300 ease-in-out">
           {visibleChildren.map((child, index) => (
-            <div key={index} className="min-w-[250px] transform transition-transform duration-300 ease-in-out">
+            <div
+              key={index}
+              className="min-w-[250px] transform transition-transform duration-300 ease-in-out"
+            >
               {child}
             </div>
           ))}
         </div>
         {currentStep > 0 && (
-          <button className="absolute left-0 top-0 bottom-0 flex items-center justify-center bg-gradient-to-l from-transparent to-gray-50 p-4" onClick={() => handleStep(-1)}>
-            <BsArrowLeftCircle className='w-6 h-6'/>
+          <button
+            className="absolute left-0 top-0 bottom-0 flex items-center justify-center bg-gradient-to-l from-transparent to-gray-50 p-4"
+            onClick={() => handleStep(-1)}
+          >
+            <BsArrowLeftCircle className="w-6 h-6" />
           </button>
         )}
         {currentStep < totalSteps - 1 && (
-          <button className="absolute right-0 top-0 bottom-0 flex items-center justify-center bg-gradient-to-r from-transparent to-gray-50 p-4" onClick={() => handleStep(1)}>
-            <BsArrowRightCircle className='w-6 h-6'/>
+          <button
+            className="absolute right-0 top-0 bottom-0 flex items-center justify-center bg-gradient-to-r from-transparent to-gray-50 p-4"
+            onClick={() => handleStep(1)}
+          >
+            <BsArrowRightCircle className="w-6 h-6" />
           </button>
         )}
       </div>
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import { connect } from "react-redux";
 import ImgCarousal from "../component/ImgCarousal";
 import { appConstants } from "../enum/constants";
@@ -7,103 +7,145 @@ import SnapCarousal from "../component/SnapCarousal";
 import Cards from "../component/Cards";
 
 const ProductDetail = (props) => {
-    const { id } = useParams();
-    const [item, setItem] = useState([]);
-    const [loading, setLoading] = useState(true);
-    
-    useEffect(() => {
-        setTimeout(() => {
-            setItem(props.items || []);
-            setLoading(props.loading);
-        }, 1000);
-    }, [props.items, props.loading]);
+  const { id } = useParams();
+  const [item, setItem] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    return (
-        <>
-        {
-            loading ? ( // Display loading skeleton when loading is true
-                <div className="container mx-auto p-4 animate-pulse">
-                    <div className="sticky top-0">
-                        <section role="main" className="flex flex-col md:flex-row gap-4 items-start">
-                            <div className="w-64 h-48 bg-gray-300 rounded-md"></div> {/* Loading image skeleton */}
-                            <div className="md:ml-8 mt-4 md:mt-0 w-full">
-                                <div className="h-6 w-3/4 bg-gray-300 rounded-md mb-2"></div> {/* Loading title skeleton */}
-                                <div className="h-4 w-1/2 bg-gray-300 rounded-full mb-2"></div> {/* Loading category skeleton */}
-                                <div className="flex items-center mt-4">
-                                    <div className="h-8 w-24 bg-gray-300 rounded-md mr-2"></div> {/* Loading price skeleton */}
-                                    <div className="h-4 w-16 bg-gray-300 rounded-md"></div> {/* Loading old price skeleton */}
-                                </div>
-                                <div className="mt-6 flex space-x-4">
-                                    <div className="h-12 w-24 bg-gray-300 rounded-md"></div> {/* Loading Buy Now button skeleton */}
-                                    <div className="h-12 w-24 bg-gray-300 rounded-md"></div> {/* Loading Add to Cart button skeleton */}
-                                </div>
-                            </div>
-                        </section>
-                    </div>
-                    <div className="mt-8">
-                        <div className="h-4 w-3/4 bg-gray-300 rounded-md mb-2"></div> {/* Loading description title skeleton */}
-                        <div className="h-4 w-3/4 bg-gray-300 rounded-md mb-4"></div> {/* Loading description skeleton */}
-                        <div className="h-4 w-2/3 bg-gray-300 rounded-md mb-4"></div> {/* Loading description skeleton */}
-                        <div className="h-4 w-4/5 bg-gray-300 rounded-md mb-4"></div> {/* Loading description skeleton */}
-                    </div>
+  useEffect(() => {
+    setTimeout(() => {
+      setItem(props.items || []);
+      setLoading(props.loading);
+    }, 1000);
+  }, [props.items, props.loading]);
+
+  return (
+    <>
+      {loading ? ( // Display loading skeleton when loading is true
+        <div className="container mx-auto p-4 animate-pulse">
+          <div className="sticky top-0">
+            <section
+              role="main"
+              className="flex flex-col md:flex-row gap-4 items-start"
+            >
+              <div className="w-64 h-48 bg-gray-300 rounded-md"></div>{" "}
+              {/* Loading image skeleton */}
+              <div className="md:ml-8 mt-4 md:mt-0 w-full">
+                <div className="h-6 w-3/4 bg-gray-300 rounded-md mb-2"></div>{" "}
+                {/* Loading title skeleton */}
+                <div className="h-4 w-1/2 bg-gray-300 rounded-full mb-2"></div>{" "}
+                {/* Loading category skeleton */}
+                <div className="flex items-center mt-4">
+                  <div className="h-8 w-24 bg-gray-300 rounded-md mr-2"></div>{" "}
+                  {/* Loading price skeleton */}
+                  <div className="h-4 w-16 bg-gray-300 rounded-md"></div>{" "}
+                  {/* Loading old price skeleton */}
                 </div>
-            ) : (
-                item.filter(itm => itm.id === id).map(itm => (
-                    <div className="container mx-auto p-4 gap-8 flex flex-col" key={itm.id}>
-                        <section role="main" className="grid md:grid-cols-2 gap-4 items-start">
-                            <ImgCarousal images={itm.images} />
-                            <div className="md:ml-8 mt-4 md:mt-0">
-                                <h1 className="text-3xl font-semibold mb-2 font-bold">{itm.name}</h1>
-                                <p>{itm.description}</p>
-                                <div className="flex items-center mt-4">
-                                    <span className="text-2xl font-semibold text-green-600">{itm.oldprice}</span>
-                                    <span className="text-lg text-gray-600 ml-2 line-through">{itm.oldprice}</span>
-                                </div>
-                                <div className="mt-6 space-x-4">
-                                    <button className="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600 transition duration-300 ease-in-out">Buy Now</button>
-                                    <button className="bg-green-500 text-white px-6 py-3 rounded-md hover:bg-green-600 transition duration-300 ease-in-out" onClick={() => props.addToCart(item)}>Add to Cart</button>
-                                </div>
-                            </div>
-                        </section>
-                        <div>
-                            <h2 className="text-lg font-semibold mb-2 font-bold">Product Description</h2>
-                            <div className="text-gray-800" dangerouslySetInnerHTML={{ __html: itm.descriptiondetail }} />
-                        </div>
-                        <div>
-                            <SnapCarousal title="Featured Items" showMoreLink="/more-items">
-                            {
-                                item && item.length > 0 ? (
-                                    item.map((item, index) => (
-                                        <Cards key={index} id={item.id} title={item.name} images={item.images} desc={item.description} oldprice={item.oldprice} newprice={item.newprice} />
-                                    ))
-                                ) : (
-                                    <p>No items</p>
-                                )
-                            }
-                            </SnapCarousal>
-                        </div>
-                    </div>
-                ))
-            )
-        }
-        </>
-    );
-}
+                <div className="mt-6 flex space-x-4">
+                  <div className="h-12 w-24 bg-gray-300 rounded-md"></div>{" "}
+                  {/* Loading Buy Now button skeleton */}
+                  <div className="h-12 w-24 bg-gray-300 rounded-md"></div>{" "}
+                  {/* Loading Add to Cart button skeleton */}
+                </div>
+              </div>
+            </section>
+          </div>
+          <div className="mt-8">
+            <div className="h-4 w-3/4 bg-gray-300 rounded-md mb-2"></div>{" "}
+            {/* Loading description title skeleton */}
+            <div className="h-4 w-3/4 bg-gray-300 rounded-md mb-4"></div>{" "}
+            {/* Loading description skeleton */}
+            <div className="h-4 w-2/3 bg-gray-300 rounded-md mb-4"></div>{" "}
+            {/* Loading description skeleton */}
+            <div className="h-4 w-4/5 bg-gray-300 rounded-md mb-4"></div>{" "}
+            {/* Loading description skeleton */}
+          </div>
+        </div>
+      ) : (
+        item
+          .filter((itm) => itm.id === id)
+          .map((itm) => (
+            <div
+              className="container mx-auto p-4 gap-8 flex flex-col"
+              key={itm.id}
+            >
+              <section
+                role="main"
+                className="grid md:grid-cols-2 gap-4 items-start"
+              >
+                <ImgCarousal images={itm.images} />
+                <div className="md:ml-8 mt-4 md:mt-0">
+                  <h1 className="text-3xl font-semibold mb-2 font-bold">
+                    {itm.name}
+                  </h1>
+                  <p>{itm.description}</p>
+                  <div className="flex items-center mt-4">
+                    <span className="text-2xl font-semibold text-green-600">
+                      {itm.oldprice}
+                    </span>
+                    <span className="text-lg text-gray-600 ml-2 line-through">
+                      {itm.oldprice}
+                    </span>
+                  </div>
+                  <div className="mt-6 space-x-4">
+                    <button className="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600 transition duration-300 ease-in-out">
+                      Buy Now
+                    </button>
+                    <button
+                      className="bg-green-500 text-white px-6 py-3 rounded-md hover:bg-green-600 transition duration-300 ease-in-out"
+                      onClick={() => props.addToCart(item)}
+                    >
+                      Add to Cart
+                    </button>
+                  </div>
+                </div>
+              </section>
+              <div>
+                <h2 className="text-lg font-semibold mb-2 font-bold">
+                  Product Description
+                </h2>
+                <div
+                  className="text-gray-800"
+                  dangerouslySetInnerHTML={{ __html: itm.descriptiondetail }}
+                />
+              </div>
+              <div>
+                <SnapCarousal title="Featured Items" showMoreLink="/more-items">
+                  {item && item.length > 0 ? (
+                    item.map((item, index) => (
+                      <Cards
+                        key={index}
+                        id={item.id}
+                        title={item.name}
+                        images={item.images}
+                        desc={item.description}
+                        oldprice={item.oldprice}
+                        newprice={item.newprice}
+                      />
+                    ))
+                  ) : (
+                    <p>No items</p>
+                  )}
+                </SnapCarousal>
+              </div>
+            </div>
+          ))
+      )}
+    </>
+  );
+};
 
 function mapStateToProps(state) {
-    return {
-        items: state.items.products,
-        loading: state.items.loading
-    }
+  return {
+    items: state.items.products,
+    loading: state.items.loading,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        addToCart: (item) => dispatch({ type: appConstants.ADD_TO_CART, payload: item })
-    }
+  return {
+    addToCart: (item) =>
+      dispatch({ type: appConstants.ADD_TO_CART, payload: item }),
+  };
 }
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(ProductDetail);
+export default connect(mapStateToProps, mapDispatchToProps)(ProductDetail);
