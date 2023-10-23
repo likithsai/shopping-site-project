54e1d5b4111b635f537195a7874a62b89c166102
likith sai <likithsai13@gmail.com>
2023-10-23 10:18:28 +0530

    minor code changes for snapcarousal

--- src/component/SnapCarousal.js
+++ src/component/SnapCarousal.js
@@ -1,31 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom'; // Import NavLink from your router library
 import { BsArrowRightCircle } from "react-icons/bs";
 
-const SnapCarousel = ({ title, children, maxVisibleItems }) => {
-  const visibleChildren = children.slice(0, maxVisibleItems);
-
+const SnapCarousel = ({ title, children }) => {
   return (
     <div className="w-full flex flex-col gap-2">
       <div className='flex items-center justify-between'>
-      {
-        title && <p className="text-lg font-semibold mb-2 font-bold">{title}</p>
-      }
-      {
-        children.length > maxVisibleItems && (
-          <div className='flex items-center gap-2'>
-            <button className="text-blue-500 hover:underline focus:outline-none">Show More</button>
+        {title && <p className="text-lg font-semibold mb-2 font-bold">{title}</p>}
+        <div className='flex items-center gap-2'>
+          <NavLink to="#" className="text-blue-500 hover:underline focus:outline-none flex items-center gap-2">
+            <span>Show More</span>
             <BsArrowRightCircle />
-          </div>
-        )
-      }
+          </NavLink>
+        </div>
       </div>
       <div className="relative w-full overflow-hidden">
         <div className="flex gap-2 overflow-x-auto">
-          {visibleChildren.map((child, index) => (
-            <div key={index} className="snap-center min-w-[250px]">
-              {child}
-            </div>
-          ))}
+          {
+            children.map((child, index) => (
+              <div key={index} className="snap-center min-w-[250px]">
+                {child}
+              </div>
+            ))
+          }
         </div>
       </div>
     </div>
